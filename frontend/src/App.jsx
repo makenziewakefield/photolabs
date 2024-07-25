019cd4a6ee38d1794e424499bfa4ad5f41bbce59
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-07-25 16:51:40 -0600

    Modified file to display multiple instances of the component by using the map method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,46 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: '1',
-  location: {
-    city: 'Montreal',
-    country: 'Canada'
+const photos = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: 'Joe Example',
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`
-};
+  {
+    id: "2",
+    location: {
+      city: "Toronto",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+    username: "Jane Doe",
+    profile: `${process.env.PUBLIC_URL}/profile-2.jpg`,
+  },
+  {
+    id: "3",
+    location: {
+      city: "Vancouver",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    username: "John Smith",
+    profile: `${process.env.PUBLIC_URL}/profile-3.jpg`,
+  },
+];
 
-// Note: Rendering a single component to build components in isolation
+
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem}/>
+      {photos.map((photo) => (
+      <PhotoListItem key={photo.id} photo={photo} />
+    ))}
     </div>
   );
 };
