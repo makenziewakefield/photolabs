d3a638ee211c6e3ea6cc1b664382468078ec5585
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-07-26 15:30:58 -0600

    Add TopicList and TopicListItem compnents with styles

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -8,10 +8,10 @@ const sampleDataForTopicListItem = {
   label: "Nature",
 };
 
-const TopicListItem = () => {
+const TopicListItem = ({ id, slug, title }) => {
   return (
-    <div className="topic-list__item">
-      {/* Insert React */}
+    <div className="topic-list__item" key={id}>
+      <span>{title}</span>
     </div>
   );
 };
