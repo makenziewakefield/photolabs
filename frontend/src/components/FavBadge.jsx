2e9ed23a996fcba9d4f1261d4f3a709f11efdb60
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-07-29 17:54:45 -0600

    Troubleshooting fav badge notification, still in progress

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,7 +3,7 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ favoritedPhotos, hasFavorites }) => {
+const FavBadge = ({ hasFavorites }) => {
 
   return (
     <div className={`fav-badge ${hasFavorites ? 'fav-badge--active' : ''}`}>
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,12 +3,12 @@ import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = ({ topics, favoritedPhotos, hasFavorites }) => {
+const TopNavigation = ({ topics, hasFavorites }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics}/>
-      <FavBadge favoritedPhotos={favoritedPhotos} hasFavorites={hasFavorites}/>
+      <FavBadge hasFavorites={hasFavorites}/>
     </div>
   )
 }
