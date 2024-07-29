48f6a59c58cef4df2ff93e2b933d72643f4b7f54
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-07-29 16:18:55 -0600

    Implement global state to track liked photos

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopNavigationBar from './TopNavigationBar';
 import PhotoList from './PhotoList';
 import TopicList from './TopicList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = ({ photos, topics }) => {
+  // State to track favorited photos
+  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
+
+  // Function to toggle favorite status of a photo
+  const toggleFavorite = (photoId) => {
+    setFavoritedPhotos(prev =>
+      prev.includes(photoId)
+        ? prev.filter(id => id !== photoId)
+        : [...prev, photoId]
+    );
+  };
+
   return (
     <div className="home-route">
-      <TopNavigationBar topics={topics}/>
-      <PhotoList photos={photos}/>
+      <TopNavigationBar
+        topics={topics}
+        favoritedPhotos={favoritedPhotos}
+      />
+      <PhotoList
+        photos={photos}
+        favoritedPhotos={favoritedPhotos}
+        toggleFavorite={toggleFavorite}
+      />
     </div>
   );
 };
