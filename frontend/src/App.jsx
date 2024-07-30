ae4258d573b434aa1b6a35c7335ab421598daa77
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-07-30 17:52:13 -0600

    Display selected photo and similar photos in modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,22 +8,32 @@ import topics from 'mocks/topics';
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [similarPhoto, setSimilarPhotos] = useState([]);
 
-  const handlePhotoClick = photo => {
-    setSelectedPhoto(photo);
+  // Function to handle photo click and open modal
+  const handleOpenModal = (selectedPhoto) => {
+    setSelectedPhoto(selectedPhoto);
+    setSimilarPhotos(selectedPhoto.similar_photos);
     setIsModalOpen(true);
   };
 
+  // function to handle closing the modal
   const handleCloseModal = () => {
     setIsModalOpen(false);
     setSelectedPhoto(null);
+    setSimilarPhotos([]);
   }
 
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} onPhotoClick={handlePhotoClick}/>
-      <PhotoDetailsModal isOpen={isModalOpen} onClose={handleCloseModal} photo={selectedPhoto}/>
-      </div>
+      <HomeRoute photos={photos} topics={topics} onPhotoClick={handleOpenModal} />
+      <PhotoDetailsModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        photo={selectedPhoto}
+        similarPhotos={similarPhoto}
+      />
+    </div>
   );
 };
 
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,17 +2,57 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from 'components/PhotoList';
 
-const PhotoDetailsModal = ({ isOpen, onClose, photo }) => {
+const PhotoDetailsModal = ({ isOpen, onClose, photo, similarPhotos }) => {
   console.log('Selected photo data:', photo);
 
   if (!isOpen) return null;
+
+  const similarPhotosArray = Object.values(similarPhotos);
 
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <div className="photo-details-modal__content">
+        {photo ? (
+          <>
+            <img
+              src={photo.urls.regular}
+              alt={photo.description || 'Selected photo'}
+              className="photo-details-modal__image"
+            />
+            <div className="photo-details-modal__photographer-details">
+            <img
+                src={photo.user.profile}
+                alt={photo.user.name}
+                className="photo-details-modal__photographer-profile"
+              />
+              <div className="photo-details-modal__user-details">
+                <div className="photo-details-modal__photographer-info">{photo.user.name}</div>
+                <div className="photo-details-modal__photographer-location">
+                  {photo.location.city}, {photo.location.country}
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <p>No photo data available</p>
+        )}
+        <h3 className='photo-details-modal_similar-photos'>Similar Photos</h3>
+        {similarPhotosArray.length > 0 ? (
+          <PhotoList
+            photos={similarPhotosArray}
+            favoritedPhotos={[]}
+            toggleFavorite={() => {}}
+            onPhotoClick={() => {}}
+          />
+        ) : (
+          <p>No similar photos available</p>
+        )}
+      </div>
     </div>
   )
 };
