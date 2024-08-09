565af6b63b0566c00f250fc33d5a9ef29d9e3aef
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-08-08 20:06:26 -0600

    Change location of HomeRoute.jsx file to routes directory

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = ({ photos, topics, handlePhotoClick, handleTopicClick, favoritedPhotos, toggleFavorite }) => {
+
+  // Boolean indicating if there are favorited photos
+  const hasFavorites = favoritedPhotos.length > 0;
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar
+        topics={topics}
+        favoritedPhotos={favoritedPhotos}
+        hasFavorites={hasFavorites}
+        handleTopicClick={handleTopicClick}
+      />
+      <PhotoList
+        photos={photos}
+        favoritedPhotos={favoritedPhotos}
+        toggleFavorite={toggleFavorite}
+        handlePhotoClick={handlePhotoClick}
+      />
     </div>
   );
 };
