138a3cb8370fb5a235b288f088b9bc3f00ff9fda
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-08-13 15:15:59 -0600

    Implement LikedPhotosModal component to display a modal with favorited photos

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,12 +5,14 @@ import '../styles/TopNavigationBar.scss'
 
 
 // Responsible for rendering nav bar, including the logo, topic list, and fav bage icon
-const TopNavigation = ({ topics, hasFavorites, handleTopicClick }) => {
+const TopNavigation = ({ topics, hasFavorites, handleTopicClick, handleFavIconClick }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} onTopicClick={handleTopicClick} />
-      <FavBadge hasFavorites={hasFavorites} />
+      <button className='fav-icon-button' onClick={handleFavIconClick}>
+        <FavBadge hasFavorites={hasFavorites} />
+      </button>
     </div>
   )
 }
